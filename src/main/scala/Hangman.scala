d452d9b6119872b1227694dcf75311e1cfce05e6
calinu4 <calinu_4u@yahoo.com>
2017-06-28 16:54:39 +0000

    Hangman 3 modes game

--- src/main/scala/Hangman.scala
+++ src/main/scala/Hangman.scala
@@ -22,77 +22,81 @@ object Hangman {
     Hangman.connectToDatabase()
     Hangman.retrieveData("words")
     Hangman.mainMenu()
-    println("Selected word was: "+selectedWord)
+    println("Selected word was: " + selectedWord)
   }
-  def mainMenu()={
-    print("Type a number to start game: \n"+
-      "1.EASY MODE \n"+"2.MEDIUM MODE\n"+"3.HARD MODE\n"+"4.QUIT Game")
+
+  def mainMenu() = {
+    print("Type a number to start game: \n" +
+      "1.EASY MODE \n" + "2.MEDIUM MODE\n" + "3.HARD MODE\n" + "4.QUIT Game")
     var sc = new Scanner(System.in)
-    var notChosen=true
-     while(notChosen){
-       val option=sc.next()
-       option match{
-         case "1"=>notChosen=false;chooseWord(1)
-         case "2"=>notChosen=false;chooseWord(2)
-         case "3"=>notChosen=false;chooseWord(3)
-         case "4"=>System.exit(0)
-         case _=>println("Invalid Input. Try again!")
-       }
-     }
-  }
-  def drawHangman()={
-    count match{
-      case 0=> print(" _________     \n");print("|         |    \n")
-      case 1=> print(" _________     \n");print("|         |    \n");print("|         0    \n")
-      case 2=> print(" _________     \n");print("|         |    \n");print("|         0    \n");print("|        / \n")
-      case 3=> print(" _________     \n");print("|         |    \n");print("|         0    \n");print("|        /|\\ \n")
-      case 4=> print(" _________     \n");print("|         |    \n");print("|         0    \n");print("|        /|\\ \n");print("|        /  \n")
-      case _=> print(" _________     \n");print("|         |    \n");print("|         0    \n");print("|        /|\\ \n");print("|        / \\ \n")
+    var notChosen = true
+    while (notChosen) {
+      val option = sc.next()
+      option match {
+        case "1" => notChosen = false; chooseWord(1)
+        case "2" => notChosen = false; chooseWord(2)
+        case "3" => notChosen = false; chooseWord(3)
+        case "4" => System.exit(0)
+        case _ => println("Invalid Input. Try again!")
+      }
     }
   }
+
+  def drawHangman() = {
+    count match {
+      case 0 => print(" _________     \n"); print("|         |    \n")
+      case 1 => print(" _________     \n"); print("|         |    \n"); print("|         0    \n")
+      case 2 => print(" _________     \n"); print("|         |    \n"); print("|         0    \n"); print("|        / \n")
+      case 3 => print(" _________     \n"); print("|         |    \n"); print("|         0    \n"); print("|        /|\\ \n")
+      case 4 => print(" _________     \n"); print("|         |    \n"); print("|         0    \n"); print("|        /|\\ \n"); print("|        /  \n")
+      case _ => print(" _________     \n"); print("|         |    \n"); print("|         0    \n"); print("|        /|\\ \n"); print("|        / \\ \n")
+    }
+  }
+
   def readInput() = {
-    val wordChars=selectedWord.toCharArray
+    val wordChars = selectedWord.toCharArray
     var sc = new Scanner(System.in)
-    while (checkGameOver() == false&&won==false) {
-      val c= sc.next().toCharArray.head
-      guesses+=c
-      if(checkGuess(c)){
-        for(i<-0 until wordChars.length-1)
-          if(wordChars(i)==c)
-            dashedWord(i)=c
-      }else {count+=1;drawHangman()}
-      won=checkWin()
+    while (checkGameOver() == false && won == false) {
+      val c = sc.next().toCharArray.head
+      guesses += c
+      if (checkGuess(c)) {
+        for (i <- 0 until wordChars.length - 1)
+          if (wordChars(i) == c)
+            dashedWord(i) = c
+      } else {
+        count += 1; drawHangman()
+      }
+      won = checkWin()
       displayWord()
     }
     checkWin()
-    if(won)
+    if (won)
       println("YOU WON!")
     else
       println("Life is cruel. You are dead. CRUCIFIXION killed you.")
   }
 
-  def checkGuess(c:Char)={
-    if(selectedWord.contains(c))
+  def checkGuess(c: Char) = {
+    if (selectedWord.contains(c))
       true
     else
       false
 
   }
+
   //Display dashed word
   def displayWord() = {
     dashedWord.foreach(c => print(c + " "))
     println("Guesses: " + guesses.mkString)
-    println("Remaining incorrect guesses: "+(5-count))
-    //println(words.length)
+    println("Remaining incorrect guesses: " + (5 - count))
   }
 
   def checkWin() = {
-    val w=dashedWord.mkString.trim()
-    if (w.contains('_')==false)
+    val w = dashedWord.mkString.trim()
+    if (w.contains('_') == false)
       true
     else
       false
-
   }
 
   def checkGameOver() = {
@@ -103,17 +107,16 @@ object Hangman {
   }
 
   //Word selector by computer
-  def chooseWord(choice:Int) = {
-    choice match{
-      case 1=> val number = scala.util.Random.nextInt(words1.length - 1);selectedWord = words1(number)
-      case 2=>val number = scala.util.Random.nextInt(words2.length - 1);selectedWord = words2(number)
-      case 3=>val number = scala.util.Random.nextInt(words3.length - 1);selectedWord = words3(number)
+  def chooseWord(choice: Int) = {
+    choice match {
+      case 1 => val number = scala.util.Random.nextInt(words1.length - 1); selectedWord = words1(number)
+      case 2 => val number = scala.util.Random.nextInt(words2.length - 1); selectedWord = words2(number)
+      case 3 => val number = scala.util.Random.nextInt(words3.length - 1); selectedWord = words3(number)
     }
-    for (i <- 0 until selectedWord.length-1)
+    for (i <- 0 until selectedWord.length - 1)
       dashedWord += '_'
     displayWord()
     readInput()
-
   }
 
   //Connect to database
@@ -140,14 +143,13 @@ object Hangman {
       val statement = connection.createStatement()
       val resultSet = statement.executeQuery(s"SELECT * FROM $table")
       while (resultSet.next()) {
-        val res=resultSet.getString("name")
-        if(res.length>=8)
-          words1+=res
+        val res = resultSet.getString("name")
+        if (res.length >= 8)
+          words1 += res
+        else if (res.length <= 7 && res.length >= 5)
+          words2 += res
         else
-          if(res.length<=7&&res.length>=5)
-          words2+=res
-        else
-          words3+=res
+          words3 += res
 
       }
       println("Words retrieved from database")
