47c8e8735f4c2aeca9c21ef21ba4a04fbbb96183
calinu4 <calinu_4u@yahoo.com>
2017-06-28 16:43:48 +0000

    Hangman 3 modes game

--- src/main/scala/Hangman.scala
+++ src/main/scala/Hangman.scala
@@ -8,7 +8,9 @@ import scala.collection.mutable.ListBuffer
 
 object Hangman {
   var connection: Connection = null
-  var words: ListBuffer[String] = new ListBuffer[String]()
+  var words1: ListBuffer[String] = new ListBuffer[String]()
+  var words2: ListBuffer[String] = new ListBuffer[String]()
+  var words3: ListBuffer[String] = new ListBuffer[String]()
   var selectedWord: String = ""
   var gameOver: Boolean = false
   var won: Boolean = false
@@ -19,12 +21,24 @@ object Hangman {
   def main(args: Array[String]): Unit = {
     Hangman.connectToDatabase()
     Hangman.retrieveData("words")
-    Hangman.chooseWord()
-    //println(selectedWord)
-    Hangman.displayWord()
-    Hangman.readInput()
-
-
+    Hangman.mainMenu()
+    println("Selected word was: "+selectedWord)
+  }
+  def mainMenu()={
+    print("Type a number to start game: \n"+
+      "1.EASY MODE \n"+"2.MEDIUM MODE\n"+"3.HARD MODE\n"+"4.QUIT Game")
+    var sc = new Scanner(System.in)
+    var notChosen=true
+     while(notChosen){
+       val option=sc.nextInt()
+       option match{
+         case 1=>notChosen=false;chooseWord(1)
+         case 2=>notChosen=false;chooseWord(2)
+         case 3=>notChosen=false;chooseWord(3)
+         case 4=>System.exit(0)
+         case _=>println("Invalid Input. Try again!")
+       }
+     }
   }
   def drawHangman()={
     count match{
@@ -52,9 +66,9 @@ object Hangman {
     }
     checkWin()
     if(won)
-      println("User won")
+      println("YOU WON!")
     else
-      println("Game Over")
+      println("Life is cruel. You are dead. CRUCIFIXION killed you.")
   }
 
   def checkGuess(c:Char)={
@@ -67,9 +81,8 @@ object Hangman {
   //Display dashed word
   def displayWord() = {
     dashedWord.foreach(c => print(c + " "))
-    println()
-    //println("Guesses: " + guesses.foreach(c => print(c + ", ")))
-    // println(selectedWord)
+    println("Guesses: " + guesses.mkString)
+    println("Remaining incorrect guesses: "+(5-count))
     //println(words.length)
   }
 
@@ -90,11 +103,16 @@ object Hangman {
   }
 
   //Word selector by computer
-  def chooseWord() = {
-    val number = scala.util.Random.nextInt(words.length - 1)
-    selectedWord = words(number)
+  def chooseWord(choice:Int) = {
+    choice match{
+      case 1=> val number = scala.util.Random.nextInt(words1.length - 1);selectedWord = words1(number)
+      case 2=>val number = scala.util.Random.nextInt(words2.length - 1);selectedWord = words2(number)
+      case 3=>val number = scala.util.Random.nextInt(words3.length - 1);selectedWord = words3(number)
+    }
     for (i <- 0 until selectedWord.length-1)
       dashedWord += '_'
+    displayWord()
+    readInput()
 
   }
 
@@ -122,7 +140,15 @@ object Hangman {
       val statement = connection.createStatement()
       val resultSet = statement.executeQuery(s"SELECT * FROM $table")
       while (resultSet.next()) {
-        words += resultSet.getString("name")
+        val res=resultSet.getString("name")
+        if(res.length>=8)
+          words1+=res
+        else
+          if(res.length<=7&&res.length>=5)
+          words2+=res
+        else
+          words3+=res
+
       }
       println("Words retrieved from database")
     }
